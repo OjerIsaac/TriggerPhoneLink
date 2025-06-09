19c900fb4d72e8d9f7f3cd125d152848505f4adc
OjerIsaac <isaacojerumu@gmail.com>
2025-06-09 09:43:58 +0100

    call phone link directly

--- TriggerPhoneLink/Form1.cs
+++ TriggerPhoneLink/Form1.cs
@@ -15,26 +15,10 @@ namespace TriggerPhoneLink
         {
             try
             {
-                bool launched = false;
+                // Legacy-compatible launch of Phone Link
+                Process.Start("explorer.exe", "shell:appsFolder\\Microsoft.YourPhone_8wekyb3d8bbwe!App");
 
-                // Try the URI method first
-                try
-                {
-                    Process.Start("ms-phone:");
-                    launched = true;
-                }
-                catch
-                {
-                    // URI might not be registered on all systems
-                }
-
-                // Fallback: Launch via shell:appsFolder
-                if (!launched)
-                {
-                    Process.Start("explorer.exe", "shell:appsFolder\\Microsoft.YourPhone_8wekyb3d8bbwe!App");
-                }
-
-                // Close the modal either way
+                // Close the modal immediately
                 this.Close();
             }
             catch (Exception ex)
